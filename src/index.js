bf4d047c8632bf388a1c49fbac24ea28b9c95b95
Cayo Legal <cayolegal@Cayos-MacBook-Air.local>
2022-04-23 12:22:04 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log("Hello world")
+
+const fancyFunc = () => {
+    return [1, 2];
+  };
+  
+  const [a, b] = fancyFunc();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+
+    resolve: {
+
+        extensions: ['.js']
+    }, 
+
+    module: {
+
+        rules: [
+
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }, 
+
+    plugins: [
+
+        new HtmlWebpackPlugin([
+
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
+            }
+        ])
+    ]
+
+}
