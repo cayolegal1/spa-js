4acb9e5adc197568a5e58332eb4dca1a4415622e
Cayo Legal <cayolegal@Cayos-MacBook-Air.local>
2022-04-23 14:08:01 -0400

    templates creation

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+
+    const view = `
+        <div class="characters">
+            <article class="character-item">
+                <a href="#/1/" class="">
+                    <img src=" " alt=" " title=" "/>
+                    <h2></h2>
+                </a>
+            </article>   
+        </div>
+    `
+
+    return view
+};
+
+export default Home;
+
